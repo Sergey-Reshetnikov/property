f7c36960fb07fdf92fd5fe542ee66c7e7a5a7c87
Sergey Reshetnikov <mr.sergey.reshetnikov@gmail.com>
2022-06-25 10:53:37 +0000

    внедрил компонент навигации

---
+++ local/templates/home/components/bitrix/breadcrumb/navigation/template.php
@@ -0,0 +1,55 @@
+<?php
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+/**
+ * @global CMain $APPLICATION
+ */
+
+global $APPLICATION;
+
+
+//delayed function must return a string
+if(empty($arResult))
+	return "";
+
+  $itemSize = count($arResult);
+
+  $result = '<div 
+    class="site-blocks-cover inner-page-cover overlay" 
+    style="background-image: url(' . SITE_TEMPLATE_PATH .'/images/hero_bg_2.jpg);" 
+    data-aos="fade" 
+    data-stellar-background-ratio="0.5" 
+    id="bx_breadcrumb_'.$index.'" 
+    itemprop="itemListElement" 
+    itemscope itemtype="http://schema.org/ListItem"> '.$arrow.'
+    <div class="container">
+    <div class="row align-items-center justify-content-center text-center">
+    <div class="col-md-10">
+      <h1 class="mb-2">' . $APPLICATION->GetTitle() . '</h1>
+    <div>
+    <a href="'.SITE_DIR.'">Главная страница</a>
+  ';
+  for($index = 1; $index < $itemSize; $index++) {
+    if ($arResult[$index]["LINK"] === $arResult[$index - 1]["LINK"]) {
+      continue;
+    }
+	  $title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+    if ($arResult[$index]["LINK"] !== "" && $arResult[$index]["LINK"] !== $APPLICATION->GetCurPage()) {
+      $result .= '
+        <span class="mx-2 text-white">•</span> 
+        <a href="'.$arResult[$index]["LINK"].'" 
+          <strong class="text-white">' . $title . '</strong>
+        </a>
+      ';
+    } elseif ($arResult[$index] == $arResult[(count($arResult) - 1)]) {
+      $result .= '
+        <span class="mx-2 text-white">•</span> <strong class="text-white">' . $title . '</strong>
+      ';
+    }
+  }
+  $result .= '</div></div></div></div></div>';
+  return $result;
+
+?>
+
+
--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -25,123 +25,112 @@
       Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/aos.css");
       Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/css/style.css");
     ?>
-    <div class="site-loader">
-    </div>
-    <div class="site-wrap">
-      <div id="panel">
-        <?$APPLICATION->ShowPanel();?>
-      </div>
-      <div class="site-mobile-menu">
-        <div class="site-mobile-menu-header">
-          <div class="site-mobile-menu-close mt-3">
-            <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body">
-      </div>
-    </div>
-    <!-- .site-mobile-menu -->
-    <div class="border-bottom bg-white top-bar">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-6 col-md-6">
-            <p class="mb-0">
-              <a href="#" class="mr-3"> <span class="text-black fl-bigmug-line-phone351"></span>
-                <?$APPLICATION->IncludeComponent(
-	              "bitrix:main.include",
-	              "",
-                  Array(
-                    "AREA_FILE_SHOW" => "file",
-                    "AREA_FILE_SUFFIX" => "inc",
-                    "EDIT_TEMPLATE" => "",
-                    "PATH" => "/include/header_phone.php"
-                  )
-                );?>
-              </a> 
-              <a href="#">
-                <?$APPLICATION->IncludeComponent(
-                "bitrix:main.include",
-                "",
-                  Array(
-                    "AREA_FILE_SHOW" => "file",
-                    "AREA_FILE_SUFFIX" => "inc",
-                    "EDIT_TEMPLATE" => "",
-                    "PATH" => "/include/header_email.php"
-                  )
-                );?>
-              </a>
-            </p>
-          </div>
-          <div class="col-6 col-md-6 text-right">
-            <?$APPLICATION->IncludeComponent(
-            "bitrix:main.include",
-            "",
-              Array(
-                "AREA_FILE_SHOW" => "file",
-                "AREA_FILE_SUFFIX" => "inc",
-                "EDIT_TEMPLATE" => "",
-                "PATH" => "/include/header_social.php"
-              )
-            );?>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="site-navbar">
-      <div class="container py-1">
-        <div class="row align-items-center">
-          <div class="col-8 col-md-8 col-lg-4">
-            <h1> 
-              <a href="<?=SITE_DIR?>" class="h5 text-uppercase text-black">
-                <?$APPLICATION->IncludeComponent(
-                "bitrix:main.include",
-                "",
-                  Array(
-                    "AREA_FILE_SHOW" => "file",
-                    "AREA_FILE_SUFFIX" => "inc",
-                    "EDIT_TEMPLATE" => "",
-                    "PATH" => "/include/header_logo.php"
-                  )
-                );?>
-              </a> 
-            </h1>
-          </div>
-          <div class="col-4 col-md-4 col-lg-8">
-            <?$APPLICATION->IncludeComponent("bitrix:menu", "top_multilevel", 
-              Array(
-                "ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
-                  "CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
-                  "DELAY" => "N",	// Откладывать выполнение шаблона меню
-                  "MAX_LEVEL" => "2",	// Уровень вложенности меню
-                  "MENU_CACHE_GET_VARS" => array(	// Значимые переменные запроса
-                    0 => "",
-                  ),
-                  "MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
-                  "MENU_CACHE_TYPE" => "A",	// Тип кеширования
-                  "MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
-                  "ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
-                  "USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
-              ),
-	            false
-            );?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <?
+<div class="site-loader">
+</div>
+<div class="site-wrap">
+	<div id="panel">
+		 <?$APPLICATION->ShowPanel();?>
+	</div>
+	<div class="site-mobile-menu">
+		<div class="site-mobile-menu-header">
+			<div class="site-mobile-menu-close mt-3">
+ <span class="icon-close2 js-menu-toggle"></span>
+			</div>
+		</div>
+		<div class="site-mobile-menu-body">
+		</div>
+	</div>
+	 <!-- .site-mobile-menu -->
+	<div class="border-bottom bg-white top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-6 col-md-6">
+					<p class="mb-0">
+ <a href="#" class="mr-3"> <span class="text-black fl-bigmug-line-phone351"></span>
+						<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/header_phone.php"
+	)
+);?> </a> <a href="#">
+						<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/header_email.php"
+	)
+);?> </a>
+					</p>
+				</div>
+				<div class="col-6 col-md-6 text-right">
+					 <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/header_social.php"
+	)
+);?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="site-navbar">
+		<div class="container py-1">
+			<div class="row align-items-center">
+				<div class="col-8 col-md-8 col-lg-4">
+					<h1> <a href="<?=SITE_DIR?>" class="h5 text-uppercase text-black"> <?$APPLICATION->IncludeComponent(
+	"bitrix:main.include",
+	"",
+	Array(
+		"AREA_FILE_SHOW" => "file",
+		"AREA_FILE_SUFFIX" => "inc",
+		"EDIT_TEMPLATE" => "",
+		"PATH" => "/include/header_logo.php"
+	)
+);?> </a> </h1>
+				</div>
+				<div class="col-4 col-md-4 col-lg-8">
+					 <?$APPLICATION->IncludeComponent(
+	"bitrix:menu",
+	"top_multilevel",
+	Array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "2",
+		"MENU_CACHE_GET_VARS" => array(0=>"",),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N"
+	)
+);?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+ <?
   $current_link = $APPLICATION->GetCurPage();
-  if ($current_link === "/about/"): ?>
-    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(../local/templates/home/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
-      <div class="container">
-        <div class="row align-items-center justify-content-center text-center">
-          <div class="col-md-10">
-            <h1 class="mb-2">About Us</h1>
-            <div><a href="<?=SITE_DIR?>">Home</a> <span class="mx-2 text-white">&bullet;</span> <strong class="text-white">About</strong></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  <?endif?>
-  
+  if ($current_link !== "/"): 
+
+  $APPLICATION->IncludeComponent("bitrix:breadcrumb", "navigation", Array(
+    "PATH" => "",	
+      "SITE_ID" => "s1",	
+      "START_FROM" => "0",	
+    ),
+    false
+  );?>
+
+<?endif?>
